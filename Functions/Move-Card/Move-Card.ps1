d523b828e2fe5f7eee0a133438971faf71f8a908
michaelrnz <michael.rowe@myob.com>
2016-08-18 11:21:18 +1200

    Move-Card added

---
+++ Functions/Move-Card/Move-Card.ps1
@@ -0,0 +1,48 @@
+﻿function Move-Card {
+    <#
+      .Synopsis 
+       Moves a Trello Card between lists.
+      .Description 
+       Moves a Trello Card between lists.
+    #>
+    [cmdletbinding()]
+    param (
+        [parameter(
+            Mandatory=$true,
+            Position=0
+        )]
+        $Token,
+        [parameter(
+            Mandatory=$true,
+            Position=1
+        )]
+        $CardId,
+        [parameter(
+            Mandatory=$true,
+            Position=2
+        )]
+        $TargetListId
+    )
+    begin
+    {
+    }
+    process
+    {
+        [hashtable]$Hash = @{
+            value = $TargetListId
+        }
+		$Data = $Hash | ConvertTo-Json
+        try {
+            Invoke-RestMethod -Method Put -Uri "https://api.trello.com/1/cards/$CardId/idList?token=$($Token.Token)&key=$($Token.AccessKey)" -Body $Data -ContentType "application/json"
+        }
+        catch
+        {
+            Write-Error $_
+        }
+    }
+    end
+    {
+    }
+}
+
+Export-ModuleMember Move-Card
